708ffef65db3253242861c9e92a987d3f6411f3a
luyosamson <luyosamson@gmail.com>
2022-12-25 09:43:53 +0300

    Modify seller form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -33,9 +33,12 @@ function handleAddPlant(e){
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form  onSubmit={handleAddPlant}>
-        <input type="text" name="name" placeholder="Plant name" onChange={(e)=>setName(e.target.value)} />
-        <input type="text" name="image" placeholder="Image URL" onChange={(e)=>setImage(e.target.value)}/>
-        <input type="number" name="price" step="0.01" placeholder="Price" onChange={(e)=>setPrice(e.target.value)}/>
+        <label>Product Name</label><input type="text" name="name" placeholder="Plant name" onChange={(e)=>setName(e.target.value)} />
+       <label>Price</label><input type="number" name="price" step="0.01" placeholder="Price" onChange={(e)=>setPrice(e.target.value)}/>
+          <label>Upload Image</label><input type="text" name="image" placeholder="Image URL" onChange={(e)=>setImage(e.target.value)}/>
+       {/* <label>Category</label><input type="radio">Flower</input>
+       <input type="button">Plant</input> */}
+        <label>Description</label><textarea></textarea>
         <button type="submit">Add Plant</button>
       </form>
     </div>
