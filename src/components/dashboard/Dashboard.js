b153a60562c0d5df09f1adf19256c981e7a660b0
luyosamson <luyosamson@gmail.com>
2023-03-06 22:21:05 +0300

    Modified

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,37 +1,28 @@
-import React from 'react'
-import './Dashboard.css'
+
+
+import React from 'react';
+import './Dashboard.css';
 import { NavLink } from 'react-router-dom';
 
 function Dashboard() {
   return (
-    
-        <div className='dashboard'>
-          <div className='menu'>
-            <NavLink exact to="/seller/dashboard">
-              <h4>Dashboard</h4>
-            </NavLink>
-            <NavLink exact to='/seller/dashboard/Sellerplants'>
-                <h4>Plants</h4>
-
-            </NavLink>
-            <NavLink exact to='/seller/dashboard/Sellerflowers'
-            >
-                <h4>Flowers</h4>
-            </NavLink>
-
-            <NavLink exact to='/seller/dashboard/NewPlantForm'>
-
-                <h4>Post Product</h4>
-
-            </NavLink>
-           
-        </div>     
-        </div>
-        
-
-    
-  )
+    // <div className='dashboard'>
+      <div className='menu-container'>
+        <NavLink exact to="/seller/dashboard" className='menu-item'>
+          <h4>Dashboard</h4>
+        </NavLink>
+        <NavLink exact to='/seller/dashboard/Sellerplants' className='menu-item'>
+          <h4>Plants</h4>
+        </NavLink>
+        <NavLink exact to='/seller/dashboard/Sellerflowers' className='menu-item'>
+          <h4>Flowers</h4>
+        </NavLink>
+        <NavLink exact to='/seller/dashboard/NewPlantForm' className='menu-item'>
+          <h4>Post Product</h4>
+        </NavLink>
+      </div>
+    // </div>
+  );
 }
 
-export default Dashboard
-
+export default Dashboard;
