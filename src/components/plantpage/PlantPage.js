9ee3f4f0cca040ccb0fe72101b0fd0918e342ac1
luyosamson <luyosamson@gmail.com>
2023-03-20 17:57:29 +0300

    Modified

--- src/components/plantpage/PlantPage.js
+++ src/components/plantpage/PlantPage.js
@@ -1,11 +1,14 @@
 import React,{useState,useEffect} from 'react'
 import PlantList from '../plantlist/PlantList'
 import Pagination from '../pagination/Pagination'
+import Search from '../search/Search'
+import './Plantpage.css'
 
 function PlantPage({handleClick}) {
   const [items,setItems]=useState([])
   const [currentPage,setCurrentPage]=useState(1)
   const [postPerPage,setPostperpage]=useState(8)
+   const [searchTerm, setSearchTerm] = useState("");
   
 
   
@@ -16,14 +19,43 @@ useEffect(()=>{
   .then((items)=>setItems(items))
 },[])
 
+  const handleChange = (value) => {
+    setSearchTerm(value);
+  };
+
 
 const lastPostindex=currentPage*postPerPage;
 const firstPostindex=lastPostindex-postPerPage;
-const currentPosts=items.slice(firstPostindex,lastPostindex);
+
+ const filteredList = items.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+const currentPosts=filteredList.slice(firstPostindex,lastPostindex);
 
 
   return (
     <div>
+
+      <div className='plants-header'>
+        <label>Filter By:</label>
+        <select>
+          <option>All</option>
+          <optgroup label='Type'>
+            <option>Succulent Plants</option>
+            <option>Floor Plants</option>
+            <option>Outdoor Plants</option>
+            <option>Bonsai</option>
+            <option>Flowering Plants</option>
+            <option>Herbs</option>
+            <option>Tree Plants</option>
+            <option>Gift Plants</option>
+          </optgroup>
+        </select>
+         <Search onSearch={searchTerm} 
+        onChangeSearch={handleChange}
+         />
+      </div>
   <PlantList items={currentPosts}
   handleClick={handleClick}/>  
 
