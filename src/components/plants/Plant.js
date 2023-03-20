b335e6a6c7781043d8188f64e60926c033f8ce5d
luyosamson <luyosamson@gmail.com>
2023-03-20 13:26:30 +0300

    Modified

--- src/components/plants/Plant.js
+++ src/components/plants/Plant.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import PlantPage from '../plantpage/PlantPage'
+import Search from '../search/Search'
 import './Plants.css'
 
 function Plant({handleClick}) {
   return (
     <div className='plants'>
-      Filter By<select>
-        <option>All</option>
-        <optgroup label='Type'>
-          <option>Succulent Plants</option>
-          <option>Floor Plants</option>
-          <option>Outdoor Plants</option>
-          <option>Bonsai</option>
-          <option>Flowering Plants</option>
-          <option>Herbs</option>
-           <option>Tree Plants</option>
-             <option>Gift Plants</option>
-
-        </optgroup>
-      </select>
-      <PlantPage  handleClick={handleClick}/>
-      
+      <div className='plants-header'>
+        <label>Filter By:</label>
+        <select>
+          <option>All</option>
+          <optgroup label='Type'>
+            <option>Succulent Plants</option>
+            <option>Floor Plants</option>
+            <option>Outdoor Plants</option>
+            <option>Bonsai</option>
+            <option>Flowering Plants</option>
+            <option>Herbs</option>
+            <option>Tree Plants</option>
+            <option>Gift Plants</option>
+          </optgroup>
+        </select>
+        <Search/>
+      </div>
+      <PlantPage handleClick={handleClick}/>
     </div>
   )
 }
