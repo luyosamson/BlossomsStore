969108212c03f13c755abd716a9407502daf0c64
luyosamson <luyosamson@gmail.com>
2023-03-23 21:49:51 +0300

    Created about us

---
+++ src/components/aboutus/Aboutus.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Aboutus.css'
+
+
+function Aboutus() {
+  return (
+    <div className='about-container'>
+      
+      <h2 className='about-title'>New impression gives you new ideas,and it is the same with flowers! </h2>
+      <div className='aboutus'>
+        <h2>About Us</h2>
+        <h4>
+          Blossoms is a product of PlantTech Limited.We came up with the idea 
+          to build<br></br> a marketplace for plants and flowers <br></br>in Kenya after we struggled 
+          to find an online plant vendor in Kenya<br></br> while we were looking to buy plants 
+          for our offices.<br></br> We combined our desire for nature and passion for innovation 
+          to<br></br> make buying flowers and plants in Kenya easier and tadaaa ...<br></br> Blossoms was born. 
+          A smarter way to buy, sell and discover the magic of flowers and plants.
+        </h4>
+       
+      </div>
+    </div>
+  );
+}
+
+export default Aboutus;
