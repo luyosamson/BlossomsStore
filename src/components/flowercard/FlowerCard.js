c1905b4369fef8bcc7089423b124f004c19a710e
luyosamson <luyosamson@gmail.com>
2023-03-24 22:06:10 +0300

    Modified struture of Flowercards component

--- src/components/flowercard/FlowerCard.js
+++ src/components/flowercard/FlowerCard.js
@@ -1,21 +1,20 @@
 import React from 'react'
+import './FlowerCard.css'
 
 function FlowerCard({newList,handleClick}) {
   return (
     
     <li className='card'>
 
-       <img src={newList.image} alt={newList.name}/> 
+       <img src={newList.image} alt={newList.name}/>
        <div>
-      <p>{newList.name}</p>
+      <p className='itemName'>{newList.name}</p>
       <p>Ksh.{newList.price}</p>
         <p ><span className='status'>{JSON.stringify(newList.inStock?"InStock":"SoldOut")}</span></p>
       <button onClick={()=>handleClick(newList)}>Add to Cart</button>
       
       </div>
   </li>
-
- 
   )
 }
 
