2c13568bfdc9f95f089c439e3f1b9bfc142e063e
luyosamson <luyosamson@gmail.com>
2023-03-24 22:03:12 +0300

    Modified struture of Featured component

--- src/components/featuredcard/FeaturedCard.js
+++ src/components/featuredcard/FeaturedCard.js
@@ -7,7 +7,7 @@ function FeaturedCard({lists,handleClick}) {
     <li className='Featuredcard'>
        <img src={lists.image} alt={lists.name}/> 
        <div>
-      <p>{lists.name}</p>
+      <p className='itemName'>{lists.name}</p>
       <p>Ksh.{lists.price}</p>
       <button onClick={()=>handleClick(lists)}>Add to Cart</button>
       </div>
